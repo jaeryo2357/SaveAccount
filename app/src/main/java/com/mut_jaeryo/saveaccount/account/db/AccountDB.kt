78f6fef5a6f458539d839fd8aa79294254e1d525
jaeryo2357 <jaeryo2357@naver.com>
2020-06-02 20:59:23 +0900

    Room + LiveData

--- app/src/main/java/com/mut_jaeryo/saveaccount/account/db/AccountDB.kt
+++ app/src/main/java/com/mut_jaeryo/saveaccount/account/db/AccountDB.kt
@@ -1,17 +1,36 @@
 package com.mut_jaeryo.saveaccount.account.db
 
 import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.mut_jaeryo.saveaccount.account.model.Account
+import com.mut_jaeryo.saveaccount.account.model.AccountDao
+import kotlinx.coroutines.CoroutineScope
 
-class AccountDB {
+@Database(entities = arrayOf(Account::class), version = 1, exportSchema = false)
+public abstract class AccountDB : RoomDatabase(){
+
+    abstract fun accountDao(): AccountDao
 
     companion object {
-        @Volatile private var db : Database? = null
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: AccountDB? = null
 
-        @JvmStatic fun getInstance(context : Context): Database =
-            db ?: synchronized(this) {
-                db ?: Database(context).also {
-                    db = it
-                }
+        fun getDatabase(
+            context: Context
+        ): AccountDB {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AccountDB::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
             }
+        }
     }
 }
--- app/src/main/java/com/mut_jaeryo/saveaccount/account/model/Account.kt
+++ app/src/main/java/com/mut_jaeryo/saveaccount/account/model/Account.kt
@@ -1,3 +1,13 @@
 package com.mut_jaeryo.saveaccount.account.model
 
-data class Account(val site:String,var id:String, var pwd:String)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "account_table")
+data class Account(
+    @PrimaryKey(autoGenerate = true) val id : Int,
+    @ColumnInfo(name = "site") val site : String,
+    @ColumnInfo(name = "user_id") var user_id : String,
+    @ColumnInfo(name = "user_pwd") var user_pwd : String
+)
