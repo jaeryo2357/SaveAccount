14b80103c67b94fa5727cf113e1fe3549b1168c7
jaeryo2357 <jaeryo2357@naver.com>
2020-07-14 17:34:03 +0900

    [create]: create Account Data Source

---
+++ app/src/main/java/com/mut_jaeryo/saveaccount/data/source/AccountDataSource.kt
@@ -0,0 +1,33 @@
+package com.mut_jaeryo.saveaccount.data.source
+
+import com.mut_jaeryo.saveaccount.data.Account
+
+/**
+ * Account Data를 접근하는 방법을 기술하는 interface
+ */
+interface AccountDataSource {
+
+    interface LoadAccountsCallback {
+
+        fun onAccountsLoaded(accounts: List<Account>)
+
+        fun onDataNotAvailable()
+    }
+
+    interface GetAccountsCallback {
+
+        fun onAccountsLoaded(account: Account)
+
+        fun onDataNotAvailable()
+    }
+
+    fun getAccounts(callback : LoadAccountsCallback)
+
+    fun getAccount(accountId : String, callback : GetAccountsCallback)
+
+    fun saveAccount(account: Account)
+
+    fun deleteAllAccounts()
+
+    fun deleteAccount(account : Account)
+}
