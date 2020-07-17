55f5e0664fb7b0ffd7f67a3412b25bed3e56378a
jaeryo2357 <jaeryo2357@naver.com>
2020-07-17 15:40:26 +0900

    [create]: background Thread class that DiskIO, NetworkIO, Main using by Executor

---
+++ app/src/main/java/com/mut_jaeryo/saveaccount/util/AppExecutor.kt
@@ -0,0 +1,23 @@
+package com.mut_jaeryo.saveaccount.util
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+const val THREAD_COUNT = 3
+
+open class AppExecutor constructor(
+    val disIO: Executor = DiskIOThreadExecutor(),
+    val networkIO: Executor = Executors.newFixedThreadPool(THREAD_COUNT),
+    val mainThread: Executor = MainThreadExecutor()
+) {
+    //UI 작업을 실행하는 Thread
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
+}
