8510368f6905b8d05acaadbfa523a1b406850c4b
jaeryo2357 <jaeryo2357@naver.com>
2020-07-20 17:14:21 +0900

    [create]: create a AccountsContract

--- app/src/main/java/com/mut_jaeryo/saveaccount/accounts/AccountsActivity.kt
+++ app/src/main/java/com/mut_jaeryo/saveaccount/accounts/AccountsActivity.kt
@@ -1,4 +1,4 @@
-package com.mut_jaeryo.saveaccount.account.ui
+package com.mut_jaeryo.saveaccount.accounts
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -7,7 +7,7 @@ import com.mut_jaeryo.saveaccount.R
 import com.mut_jaeryo.saveaccount.insert.ui.InsertActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class AccountsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
 
         add_Account.setOnClickListener {
-            val intent = Intent(this@MainActivity,
+            val intent = Intent(this@AccountsActivity,
                 InsertActivity::class.java)
             startActivity(intent)
         }
---
+++ app/src/main/java/com/mut_jaeryo/saveaccount/accounts/AccountsContract.java
@@ -0,0 +1,12 @@
+package com.mut_jaeryo.saveaccount.accounts;
+
+public class AccountsContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
