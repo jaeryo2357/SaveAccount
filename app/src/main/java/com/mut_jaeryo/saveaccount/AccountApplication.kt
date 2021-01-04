a6edc5a723755bdc9b35f606359fbd2923506e96
jaeryo2357 <jaeryo2357@naver.com>
2021-01-04 18:52:22 +0900

    add DayNight

--- app/src/main/java/com/mut_jaeryo/saveaccount/AccountApplication.kt
+++ app/src/main/java/com/mut_jaeryo/saveaccount/AccountApplication.kt
@@ -1,8 +1,19 @@
 package com.mut_jaeryo.saveaccount
 
 import android.app.Application
+import androidx.preference.PreferenceManager
+import com.mut_jaeryo.saveaccount.theme.ThemeHelper
+import com.mut_jaeryo.saveaccount.theme.ThemeHelper.applyTheme
 import dagger.hilt.android.HiltAndroidApp
+
 
 @HiltAndroidApp
 class AccountApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val themePref = sharedPreferences.getString("themePref", ThemeHelper.DEFAULT_MODE)
+
+        applyTheme(themePref ?: ThemeHelper.DEFAULT_MODE)
+    }
 }
